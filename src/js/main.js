d605a9c35ab79fc8050d71a4b2d0db732fd64f26
Ismael Ordás <ismael.ordas@gmail.com>
2018-07-05 09:17:45 +0200

    Change way to init component. Fix padding

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 import Accordion from "./components/c-accordion";
 
-(() => {
-    
+document.addEventListener("DOMContentLoaded", () => {
     const accordionPlaceholder = document.querySelector(".accordion-placeholder");
     new Accordion(accordionPlaceholder);
-
-})();
+});
